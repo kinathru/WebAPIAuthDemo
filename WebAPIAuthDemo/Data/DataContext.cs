ab9f8fed536124db394ab9746edd54b012136ba3
kinath <kinath.ru@gmail.com>
2025-01-02 15:57:08 -0600

    feat:implemented web api

---
+++ WebAPIAuthDemo/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebAPIAuthDemo.Data;
+
+public class DataContext : IdentityDbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+}
